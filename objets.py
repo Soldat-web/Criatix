46ef172ffc55b767ef31450d877fd680a9fb4c47
Soldat-web <adrien.naicken43@gmail.com>
2025-12-19 08:39:02 +0100

    Nouveau fichier

---
+++ objets.py
@@ -0,0 +1,37 @@
+class Foret:
+    pass
+class Arbre(Foret):
+    pass
+class Chemin(Foret):
+    pass
+
+
+class Arene:
+    pass
+
+
+
+class Defis:
+    pass
+
+
+class Coffre:
+    pass
+class Criatix:
+    pass
+
+
+
+
+
+
+class Batiments:
+    pass
+class Maison(Batiments):
+    pass
+class Hopital(Batiments):
+    pass
+class Magasins(Batiments):
+    pass
+
+
