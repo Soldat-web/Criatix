7227acf5360f57c3b8c1c7dff72ba2f0b11f8798
Soldat-web <adrien.naicken43@gmail.com>
2025-12-19 09:58:28 +0100

    adrien_class les portes ouvertes ou fermées

--- Adrien_class.py
+++ Adrien_class.py
@@ -30,7 +30,27 @@ class Criatix:
 
 
 class Batiments:
-    pass
+    def __init__(self):
+        self.porte = 0
+    
+    def porte_ouverte(self):
+        
+        if self.porte > 1:
+            self.porte = 1
+        elif self.porte < 0:
+            self.porte = 0
+
+        if self.porte_ouverte == 0:
+            print("Porte fermée")
+        else:
+            print("porte ouverte")
+
+    def porte_ouvrir(self):
+        self.porte = 1
+
+    def porte_fermer(self):
+        self.porte = 0
+
 class Maison(Batiments):
     pass
 class Hopital(Batiments):
