ede2d8b6f5dcb1c300583967a045e93303383ff4
Adrien <adrien.naicken43@gmail.com>
2026-03-30 11:48:18 +0200

    map interior 1

---
+++ assets/map/tileset/interieur/kitchen/kitchen_LRK.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="kitchen_LRK" tilewidth="16" tileheight="16" tilecount="650" columns="26">
+ <image source="kitchen_LRK.png" width="416" height="400"/>
+</tileset>
---
+++ assets/map/tileset/interieur/sol/Walls and floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Walls and floors" tilewidth="16" tileheight="16" tilecount="336" columns="24">
+ <image source="House/Walls and floors.png" width="397" height="232"/>
+</tileset>
